3c63041f877551753784c97aa7f29998c917d5ee
Thanatine <firewings89504@gmail.com>
2017-12-27 21:35:34 +0800

    preprocessing done, model left to correct

--- utils.py
+++ utils.py
@@ -1,31 +1,20 @@
 import numpy as np
 
-def prepare_batch(inputs, dim, max_sequence_length=None, emb_size = 0, time_major = False):
+def prepare_batch(inputs, dim, max_sequence_length = None):
     sequence_lengths = [len(seq) for seq in inputs]
     batch_size = len(inputs)
     
     if max_sequence_length is None:
         max_sequence_length = max(sequence_lengths)
-   
-    if dim == 3:
-        inputs_batch_major = np.zeros(shape=[batch_size, 
-                                             max_sequence_length, 
-                                             emb_size], 
-                                      dtype=np.float32)
-    elif dim == 2:
-        inputs_batch_major = np.zeros(shape=[batch_size, 
-                                             max_sequence_length], 
-                                      dtype=np.int32)
-    
+        
+    inputs_batch_major = np.zeros(shape=[batch_size, max_sequence_length], 
+                                  dtype=np.int32)
+
     for i, seq in enumerate(inputs):
         for j, element in enumerate(seq):
             inputs_batch_major[i, j] = element
 
-    # [batch_size, max_time] -> [max_time, batch_size]
-    if time_major == True:
-        return inputs_batch_major.swapaxes(0, 1), sequence_lengths
-    else:
-        return inputs_batch_major, sequence_lengths
+    return inputs_batch_major, sequence_lengths
 
         
 def batch_generator(seq, seq1, seq2, seq3):            
